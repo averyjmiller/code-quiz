9a33cc707380d09a6094c7cc211144d7fea48b0f
Avery-lab <54604339+Avery-lab@users.noreply.github.com>
2023-05-19 10:48:17 -0400

    made an object array template for each trivia question and the corrosponding answer and decoy answers

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+var trivia = [
+    {
+        question: "This is question 1?",
+        answer: "This is the correct answer for Q1.",
+        decoys: ["decoy 1", "decoy 2", "decoy 3"]
+    },
+    {
+        question: "This is question 2?",
+        answer: "This is the correct answer for Q2.",
+        decoys: ["decoy 1", "decoy 2", "decoy 3"]
+    },
+    {
+        question: "This is question 3?",
+        answer: "This is the correct answer for Q3.",
+        decoys: ["decoy 1", "decoy 2", "decoy 3"]
+    },
+    {
+        question: "This is question 4?",
+        answer: "This is the correct answer for Q4.",
+        decoys: ["decoy 1", "decoy 2", "decoy 3"]
+    }
+];
+
